2bf5733a7a91c14c6b24929d2cf0939e333f5672
mvacjar <mvacjar@gmail.com>
2023-12-28 17:44:21 +0100

    Alert Added to the Button

--- typescript-task/src/App.tsx
+++ typescript-task/src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 const App = () => {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
-    <div>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
-        My Button
-      </Button>
-    </div>
+    <>
+      <div>
+        {alertVisible && (
+          <Alert onClose={() => setAlertVisibility}>My Alert</Alert>
+        )}
+        <Button color="primary" onClick={() => setAlertVisibility(true)}>
+          My Button
+        </Button>
+      </div>
+    </>
   );
 };
 
